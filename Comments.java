3cf35c767b641e91a73f31112c9978c211b99017
Suraj-dyp <surajdypcoe@gmail.com>
2017-07-26 20:20:10 -0700

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,93 @@
+package com.tpt.bonzai;
+
+public class Comments {
+
+//	Date date = Calendar.getInstance().getTime();
+//	java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+//	preparedStatement.setDate(index, sqlDate);
+//	Finds date in given format
+//	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//	String formattedDate = dateFormat.format(date);
+//	System.out.println(formattedDate);
+	
+	
+//	private static void displayEODAttributes(ResultSet resultSet) {
+//	try {
+//		while(resultSet.next()) {
+//			System.out.print(resultSet.getString(1) + "  ");
+//			System.out.print(resultSet.getString(2) + "  ");
+//			System.out.print(resultSet.getString(3) + "  ");
+//			System.out.print(resultSet.getString(4) + "  ");
+//			System.out.print(resultSet.getString(5) + "  ");
+//			System.out.print(resultSet.getString(6) + "  ");
+//			System.out.println(resultSet.getString(7) + "  ");
+//		}
+//	} catch (SQLException e) {
+//		e.printStackTrace();
+//	}
+//}
+	
+//	private static void displayEODAttributes(ResultSet resultSet) {
+//	try {
+//		while(resultSet.next()) {
+//			System.out.print(resultSet.getString(1) + "  ");
+//			System.out.print(resultSet.getString(2) + "  ");
+//			System.out.print(resultSet.getString(3) + "  ");
+//			System.out.print(resultSet.getString(4) + "  ");
+//			System.out.print(resultSet.getString(5) + "  ");
+//			System.out.print(resultSet.getString(6) + "  ");
+//			System.out.print(resultSet.getString(7) + "  ");
+//			System.out.println(resultSet.getString(8) + "  ");
+//		}
+//	} catch (SQLException e) {
+//		e.printStackTrace();
+//	}
+//}
+	
+//	private static void displayProfitNLossForCommodities(ResultSet resultSet) {
+//	System.out.println("########################################");
+//	System.out.println("Commodity\tProfit/Loss");
+//	System.out.println("########################################");
+//	try {
+//		while(resultSet.next()) {
+//			System.out.print(resultSet.getString(1) + "  ");
+//			System.out.println(resultSet.getString(2) + "  ");
+//		}
+//	} catch (SQLException e) {
+//		e.printStackTrace();
+//	}
+//
+//}
+	
+	
+//	private static void displayProfitNLossForCounterpart(ResultSet resultSet) {
+//		System.out.println("######################################");
+//		System.out.println("Counterpart\tProfit/Loss");
+//		System.out.println("######################################");
+//		try {
+//			while(resultSet.next()) {
+//				System.out.print(resultSet.getString(1) + "  ");
+//				System.out.println(resultSet.getString(2) + "  ");
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	
+//	}
+
+//	private static void displayProfitNLossForCounterpartGivenDateRange(ResultSet resultSet) {
+//		System.out.println("##########################################");
+//		System.out.println("Counterpart\tDate\tProfit/Loss");
+//		System.out.println("##########################################");
+//		try {
+//			while(resultSet.next()) {
+//				System.out.print(resultSet.getString(1) + "  ");
+//				System.out.print(resultSet.getString(3) + "  ");
+//				System.out.println(resultSet.getString(2) + "  ");
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	
+//	}
+}
